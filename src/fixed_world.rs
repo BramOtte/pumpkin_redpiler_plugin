899410ab09239dea8220501877ae780fe1f53374
Bram Otte <bram@otte.biz>
2025-08-01 03:28:14 +0200

    Start translating to pumpkin block data to mchprs block data

---
+++ src/fixed_world.rs
@@ -0,0 +1,120 @@
+use mchprs_blocks::{block_entities::BlockEntity, BlockPos};
+use mchprs_world::{storage::Chunk, TickEntry, TickPriority, World};
+
+pub struct TestWorld {
+    chunks: Vec<Chunk>,
+    pub to_be_ticked: Vec<TickEntry>,
+    pub size_x: i32,
+    pub size_y: i32,
+    pub size_z: i32,
+}
+
+impl TestWorld {
+    pub fn new(size_x: i32, size_y: i32, size_z: i32) -> TestWorld {
+        let mut chunks = Vec::new();
+        for x in 0..size_x {
+            for z in 0..size_z {
+                chunks.push(Chunk::empty(x, z, size_y as usize));
+            }
+        }
+        TestWorld {
+            chunks,
+            to_be_ticked: Vec::new(),
+            size_x, size_y, size_z
+        }
+    }
+
+    fn get_chunk_index_for_chunk(&self, chunk_x: i32, chunk_z: i32) -> usize {
+        (chunk_x * self.size_z + chunk_z).unsigned_abs() as usize
+    }
+
+    fn get_chunk_index_for_block(&self, block_x: i32, block_z: i32) -> Option<usize> {
+        let chunk_x = block_x >> 4;
+        let chunk_z = block_z >> 4;
+        if chunk_x >= self.size_x || chunk_z >= self.size_z {
+            return None;
+        }
+        Some(((chunk_x * self.size_z) + chunk_z).unsigned_abs() as usize)
+    }
+}
+
+impl World for TestWorld {
+    /// Returns the block state id of the block at `pos`
+    fn get_block_raw(&self, pos: BlockPos) -> u32 {
+        let chunk_index = match self.get_chunk_index_for_block(pos.x, pos.z) {
+            Some(idx) => idx,
+            None => return 0,
+        };
+        let chunk = &self.chunks[chunk_index];
+        chunk.get_block((pos.x & 0xF) as u32, pos.y as u32, (pos.z & 0xF) as u32)
+    }
+
+    /// Sets a block in storage. Returns true if a block was changed.
+    fn set_block_raw(&mut self, pos: BlockPos, block: u32) -> bool {
+        let chunk_index = match self.get_chunk_index_for_block(pos.x, pos.z) {
+            Some(idx) => idx,
+            None => return false,
+        };
+
+        // Check to see if block is within height limit
+        if pos.y >= self.size_y * 16 || pos.y < 0 {
+            return false;
+        }
+
+        let chunk = &mut self.chunks[chunk_index];
+        chunk.set_block(
+            (pos.x & 0xF) as u32,
+            pos.y as u32,
+            (pos.z & 0xF) as u32,
+            block,
+        )
+    }
+
+    fn delete_block_entity(&mut self, pos: BlockPos) {
+        let chunk_index = match self.get_chunk_index_for_block(pos.x, pos.z) {
+            Some(idx) => idx,
+            None => return,
+        };
+        let chunk = &mut self.chunks[chunk_index];
+        chunk.delete_block_entity(BlockPos::new(pos.x & 0xF, pos.y, pos.z & 0xF));
+    }
+
+    fn get_block_entity(&self, pos: BlockPos) -> Option<&BlockEntity> {
+        let chunk_index = match self.get_chunk_index_for_block(pos.x, pos.z) {
+            Some(idx) => idx,
+            None => return None,
+        };
+        let chunk = &self.chunks[chunk_index];
+        chunk.get_block_entity(BlockPos::new(pos.x & 0xF, pos.y, pos.z & 0xF))
+    }
+
+    fn set_block_entity(&mut self, pos: BlockPos, block_entity: BlockEntity) {
+        let chunk_index = match self.get_chunk_index_for_block(pos.x, pos.z) {
+            Some(idx) => idx,
+            None => return,
+        };
+        let chunk = &mut self.chunks[chunk_index];
+        chunk.set_block_entity(BlockPos::new(pos.x & 0xF, pos.y, pos.z & 0xF), block_entity);
+    }
+
+    fn get_chunk(&self, x: i32, z: i32) -> Option<&Chunk> {
+        self.chunks.get(self.get_chunk_index_for_chunk(x, z))
+    }
+
+    fn get_chunk_mut(&mut self, x: i32, z: i32) -> Option<&mut Chunk> {
+        let chunk_idx = self.get_chunk_index_for_chunk(x, z);
+        self.chunks.get_mut(chunk_idx)
+    }
+
+    fn schedule_tick(&mut self, pos: BlockPos, delay: u32, priority: TickPriority) {
+        self.to_be_ticked.push(TickEntry {
+            pos,
+            ticks_left: delay,
+            tick_priority: priority,
+        });
+    }
+
+    fn pending_tick_at(&mut self, _pos: BlockPos) -> bool {
+        false
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
+mod fixed_world;
+
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use mchprs_blocks::blocks::{Lever, RedstoneWire, RedstoneWireSide};
+use mchprs_redpiler::{BackendVariant, Compiler, CompilerOptions};
 use pumpkin_api_macros::{plugin_impl, plugin_method};
+use pumpkin_data::{block_properties::{BlockProperties, EastWireConnection, EnumVariants, LeverLikeProperties, NorthWireConnection, RedstoneOreLikeProperties, RedstoneWireLikeProperties, SouthWireConnection, WestWireConnection}, Block};
+use mchprs_world::World;
 
+type RedstoneWireProperties = RedstoneWireLikeProperties;
+type RWallTorchProps = pumpkin_data::block_properties::FurnaceLikeProperties;
+type RTorchProps = pumpkin_data::block_properties::RedstoneOreLikeProperties;
+type RedstoneLampProperties = RedstoneOreLikeProperties;
 
 #[plugin_method]
 async fn on_load(&mut self, server: Arc<Context>) -> Result<(), String> {
@@ -12,7 +22,6 @@ async fn on_load(&mut self, server: Arc<Context>) -> Result<(), String> {
 #[inline(always)]
 async fn on_load_internal(plugin: &mut MyPlugin, server: Arc<Context>) -> Result<(), String> {
     pumpkin::init_log!(); 
-
 
     log::info!("Hello, Pumpkin!");
 
@@ -24,22 +33,30 @@ async fn on_load_internal(plugin: &mut MyPlugin, server: Arc<Context>) -> Result
     registry.register_permission(permission)?;
 
     let command = CommandTree::new(["redpiler", "rp"], "Compile redstone in selected area for faster execution") 
-        .then(literal("compile").execute(RockPaperScissorsExecutor));
+        .then(literal("compile").execute(Exe {cmd: Command::Compile, data: plugin.data.clone()}))
+        .then(literal("pos1").execute(Exe {cmd: Command::Pos1, data: plugin.data.clone()}))
+        .then(literal("pos2").execute(Exe {cmd: Command::Pos2, data: plugin.data.clone()}))
+        .then(literal("deselect").execute(Exe {cmd: Command::Deselect, data: plugin.data.clone()}))
+    ;
     
     server.register_command(command, permission_node).await;
-    println!("registered redpiler command");
+    log::info!("registered redpiler commands");
 
     Ok(())
 }
 
 #[plugin_impl]
-pub struct MyPlugin;
+pub struct MyPlugin {
+    data: Arc<RwLock<PluginData>>
+}
 
 impl MyPlugin {
     pub fn new() -> Self {
         println!("hello from redpiler plugin");
 
-        MyPlugin
+        MyPlugin {
+            data: Arc::new(RwLock::new(PluginData::default()))
+        }
     }
 }
 
@@ -49,31 +66,232 @@ impl Default for MyPlugin {
     }
 }
 
-use pumpkin::{
-    command::{ 
-        args::ConsumedArgs, dispatcher::CommandError, tree::builder::literal, tree::CommandTree,
-        CommandExecutor, CommandSender,
-    },
-    plugin::{player::player_join::PlayerJoinEvent, Context, EventHandler, EventPriority},
-    server::Server,
-};
-use pumpkin_util::{math::position::BlockPos, permission::{Permission, PermissionDefault}, PermissionLvl};
+#[derive(Default)]
+struct PluginData {
+    pos1: Option<BlockPos>,
+    pos2: Option<BlockPos>,
+}
 
-struct RockPaperScissorsExecutor; 
+use pumpkin::{
+    block::blocks::redstone::redstone_wire::RedstoneWireBlock, command::{ 
+        args::ConsumedArgs, dispatcher::CommandError, tree::{builder::literal, CommandTree}, CommandExecutor, CommandSender,
+    }, data, plugin::{player::player_join::PlayerJoinEvent, Context, EventHandler, EventPriority}, server::Server
+};
+use pumpkin_util::{math::position::BlockPos, permission::{Permission, PermissionDefault}, text::TextComponent, PermissionLvl};
+use tokio::sync::RwLock;
+
+#[derive(Debug, Clone, Copy)]
+enum Command {
+    Compile,
+    Pos1, Pos2,
+    Deselect
+}
+
+struct Exe {
+    data: Arc<RwLock<PluginData>>,
+    cmd: Command,
+}
+
+const REDSTONE_WIRE: u16 = Block::REDSTONE_WIRE.id;
+const REDSTONE_TORCH: u16 = Block::REDSTONE_TORCH.id;
+const REDSTONE_WALL_TORCH: u16 = Block::REDSTONE_WALL_TORCH.id;
+const LEVER: u16 = Block::LEVER.id;
+const REDSTONE_LAMP: u16 = Block::REDSTONE_LAMP.id;
 
 #[async_trait] 
-impl CommandExecutor for RockPaperScissorsExecutor {
+impl CommandExecutor for Exe {
     async fn execute<'a>(
         &self,
         sender: &mut CommandSender,
-        _: &Server,
-        _: &ConsumedArgs<'a>,
+        server: &Server,
+        args: &ConsumedArgs<'a>,
     ) -> Result<(), CommandError> {
-        let world = sender.world().await.unwrap();
+        let world = sender.world().await;
+        log::info!("hello execute {:?}", self.cmd);
+        log::info!("has world {:?}", world.is_some());
+        log::info!("player {:?}", sender.as_player().map(|p| p.position().to_block_pos()));
 
-        log::info!("hello execute");
-        println!("redpiler execute");
+        let (Some(world), Some(player)) = (world, sender.as_player()) else {
+            log::error!("Redpiler commands must be run by a player");
+            return Err(CommandError::PermissionDenied);
+        };
+
+        let mut data = self.data.write().await;
+
+        
+        match self.cmd {
+            Command::Compile => {
+                let (Some(p1), Some(p2)) = (data.pos1, data.pos2) else {
+                    return Err(CommandError::PermissionDenied);
+                };
+                
+                let x1 = p1.0.x.min(p2.0.x);
+                let x2 = p1.0.x.max(p2.0.x);
+                let y1 = p1.0.y.min(p2.0.y);
+                let y2 = p1.0.y.max(p2.0.y);
+                let z1 = p1.0.z.min(p2.0.z);
+                let z2 = p1.0.z.max(p2.0.z);
+                
+                let mut plot = fixed_world::TestWorld::new(x2-x1, y2-y1, z2-z1);
+                
+                let id_air = 0;
+
+                for z in z1..=z2 {
+                    for y in y1..=y2 {
+                        for x in x1..=x2 {
+                            let pos = BlockPos::new(x, y, z);
+                            let mchprs_pos = mchprs_blocks::BlockPos::new(x - x1, y - y1, z - z1);
+                            
+                            let (b, s) = world.get_block_and_state(&pos).await;
+
+                            if b.id == id_air {
+                                continue;
+                            }
+                            
+                            let mchprs_block = match b.id {
+                                REDSTONE_WIRE => {
+                                    let props = RedstoneWireProperties::from_state_id(s.id, b);
+                                    sender.send_message(TextComponent::text(format!("{:?} {:?}", pos, props))).await;   
+                                
+                                    mchprs_blocks::blocks::Block::RedstoneWire { wire: RedstoneWire::new(
+                                        match props.north {
+                                            NorthWireConnection::Up => RedstoneWireSide::Up,
+                                            NorthWireConnection::Side => RedstoneWireSide::Side,
+                                            NorthWireConnection::None => RedstoneWireSide::None,
+                                        },
+                                        match props.south {
+                                            SouthWireConnection::Up => RedstoneWireSide::Up,
+                                            SouthWireConnection::Side => RedstoneWireSide::Side,
+                                            SouthWireConnection::None => RedstoneWireSide::None,
+                                        },
+                                        match props.east {
+                                            EastWireConnection::Up => RedstoneWireSide::Up,
+                                            EastWireConnection::Side => RedstoneWireSide::Side,
+                                            EastWireConnection::None => RedstoneWireSide::None,
+                                        },
+                                        match props.west {
+                                            WestWireConnection::Up => RedstoneWireSide::Up,
+                                            WestWireConnection::Side => RedstoneWireSide::Side,
+                                            WestWireConnection::None => RedstoneWireSide::None,
+                                        },
+                                        props.power.to_index() as u8
+                                    )}
+                                }
+                                LEVER => {
+                                    let props = LeverLikeProperties::from_state_id(s.id, b);
+                                    sender.send_message(TextComponent::text(format!("{:?} {:?}", pos, props))).await;
+
+                                    mchprs_blocks::blocks::Block::Lever { lever: Lever::new(
+                                        match props.face {
+                                            pumpkin_data::block_properties::BlockFace::Floor => mchprs_blocks::blocks::LeverFace::Floor,
+                                            pumpkin_data::block_properties::BlockFace::Wall => mchprs_blocks::blocks::LeverFace::Wall,
+                                            pumpkin_data::block_properties::BlockFace::Ceiling => mchprs_blocks::blocks::LeverFace::Ceiling,
+                                        },
+                                        direction_to_mchprs(props.facing),
+                                        props.powered
+                                    )}
+                                }
+                                REDSTONE_LAMP => {
+                                    let props = RedstoneLampProperties::from_state_id(s.id, b);
+                                    sender.send_message(TextComponent::text(format!("{:?} {:?}", pos, props))).await;
+
+                                    mchprs_blocks::blocks::Block::RedstoneLamp { lit: props.lit }
+                                }
+                                REDSTONE_TORCH => {
+                                    let props = RTorchProps::from_state_id(s.id, b);
+                                    sender.send_message(TextComponent::text(format!("{:?} {:?}", pos, props))).await;
+
+                                    continue;
+                                }
+                                REDSTONE_WALL_TORCH => {
+                                    let props = RWallTorchProps::from_state_id(s.id, b);
+                                    sender.send_message(TextComponent::text(format!("{:?} {:?}", pos, props))).await;
+
+                                    continue;
+                                }
+                                _ => {
+                                    sender.send_message(TextComponent::text(format!("{:?} {:?} skipped", pos, b.name))).await;
+                                    continue;
+                                },
+                            };
+
+                            plot.set_block(mchprs_pos, mchprs_block);
+                        }
+                    }
+
+                    let min_pos = mchprs_blocks::BlockPos::new(0, 0, 0);
+                    let max_pos =  mchprs_blocks::BlockPos::new(plot.size_x-1, plot.size_y-1, plot.size_z-1);
+
+                    let mut compiler = Compiler::default();
+                    let bounds = (min_pos, max_pos);
+                    let options = CompilerOptions {
+                        optimize: true,
+                        io_only: true,
+                        wire_dot_out: true,
+                        backend_variant: BackendVariant::Direct,
+                        export_dot_graph: true,
+                        ..Default::default()
+                    };
+                    let ticks = plot.to_be_ticked.drain(..).collect();
+                    let monitor = Default::default();
+                    compiler.compile(&mut plot, bounds, options, ticks, monitor);
+                }
+
+            },
+            Command::Pos1 => {
+                data.pos1 = Some(player.position().sub_raw(0.5, 0.5, 0.5).to_block_pos());
+            },
+            Command::Pos2 => {
+                data.pos2 = Some(player.position().sub_raw(0.5, 0.5, 0.5).to_block_pos());
+            },
+            Command::Deselect => {
+                data.pos1 = None;
+                data.pos2 = None;
+            },
+        }
 
         Ok(())
+    }
+}
+
+fn facing_to_mchprs(face: pumpkin_data::block_properties::Facing) -> mchprs_blocks::BlockFacing {
+    match face {
+        pumpkin_data::block_properties::Facing::North => mchprs_blocks::BlockFacing::North,
+        pumpkin_data::block_properties::Facing::East  => mchprs_blocks::BlockFacing::East ,
+        pumpkin_data::block_properties::Facing::South => mchprs_blocks::BlockFacing::South,
+        pumpkin_data::block_properties::Facing::West  => mchprs_blocks::BlockFacing::West ,
+        pumpkin_data::block_properties::Facing::Up    => mchprs_blocks::BlockFacing::Up   ,
+        pumpkin_data::block_properties::Facing::Down  => mchprs_blocks::BlockFacing::Down ,
+    }
+}
+
+
+fn facing_to_pumpkin(face: mchprs_blocks::BlockFacing) -> pumpkin_data::block_properties::Facing {
+    match face {
+        mchprs_blocks::BlockFacing::North => pumpkin_data::block_properties::Facing::North,
+        mchprs_blocks::BlockFacing::East  => pumpkin_data::block_properties::Facing::East ,
+        mchprs_blocks::BlockFacing::South => pumpkin_data::block_properties::Facing::South,
+        mchprs_blocks::BlockFacing::West  => pumpkin_data::block_properties::Facing::West ,
+        mchprs_blocks::BlockFacing::Up    => pumpkin_data::block_properties::Facing::Up   ,
+        mchprs_blocks::BlockFacing::Down  => pumpkin_data::block_properties::Facing::Down ,
+    }
+}
+
+fn direction_to_mchprs(face: pumpkin_data::block_properties::HorizontalFacing) -> mchprs_blocks::BlockDirection {
+    match face {
+        pumpkin_data::block_properties::HorizontalFacing::North => mchprs_blocks::BlockDirection::North,
+        pumpkin_data::block_properties::HorizontalFacing::East  => mchprs_blocks::BlockDirection::East ,
+        pumpkin_data::block_properties::HorizontalFacing::South => mchprs_blocks::BlockDirection::South,
+        pumpkin_data::block_properties::HorizontalFacing::West  => mchprs_blocks::BlockDirection::West ,
+    }
+}
+
+
+fn direction_to_pumpkin(face: mchprs_blocks::BlockDirection) -> pumpkin_data::block_properties::HorizontalFacing {
+    match face {
+        mchprs_blocks::BlockDirection::North => pumpkin_data::block_properties::HorizontalFacing::North,
+        mchprs_blocks::BlockDirection::East  => pumpkin_data::block_properties::HorizontalFacing::East ,
+        mchprs_blocks::BlockDirection::South => pumpkin_data::block_properties::HorizontalFacing::South,
+        mchprs_blocks::BlockDirection::West  => pumpkin_data::block_properties::HorizontalFacing::West ,
     }
 }
